d38a0f8b5e6015d73cd4191f0ef5ccced9edacdb
alejozavala <alejo.zavala16@gmail.com>
2021-06-30 16:15:15 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var calculadora = require('./app/main');
+
+var a = 3;
+var b = 5;
+
+console.log(`sumar: ${a} + ${b} = ${calculadora.sum(a,b)}`);
+console.log(`restar: ${a} + ${b} = ${calculadora.resta(a,b)}`);
+
